e65b3348b73b2e51abcadfec3ba09bf09c2ed09b
PaladinaEug <paladina.em1@kvt.su>
2023-07-03 15:26:00 +0300

    1.5 Поправит презентер

--- src/presenter/trips-presenter.js
+++ src/presenter/trips-presenter.js
@@ -1,9 +1,7 @@
+import ListView from '../view/list-view.js';
 import EditFormView from '../view/edit-form-view';
 import PointView from '../view/point-view';
-import ListView from '../view/list-view.js';
-
 import {render} from '../render.js';
-
 import {TRIP_COUNT} from '../const.js';
 
 export default class TripsPresenter {
@@ -12,11 +10,10 @@ export default class TripsPresenter {
 
   init = (container) => {
     this.container = container;
-
+    render(this.listComponent, this.container);
     render(this.editFormComponent, this.listComponent.getElement());
-
     for (let i = 0; i < TRIP_COUNT; i++) {
-      render(new PointView (), this.container);
+      render(new PointView (), this.listComponent.getElement());
     }
   };
 }
